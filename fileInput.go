2764c973d866490ff3b47e1b8567804a0c7113d1
Aubrey Overson <aubrey.overson@gmail.com>
2016-06-19 11:51:39 -0700

    init commit - POST a string and find length of string

---
+++ fileInput.go
@@ -0,0 +1,5 @@
+package main
+
+type FileInput struct {
+	Line    string       `json:"line"`
+}
---
+++ repo.go
@@ -0,0 +1,12 @@
+package main
+
+//import "fmt"
+
+func init() {
+
+	}
+
+func FindLength(t string) int {	
+	return len(t)
+}
+
